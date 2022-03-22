b573e7954d49989b4de43b82350b69db6d95d53d
James McConnell <james.mcc90@gmail.com>
2022-03-22 10:13:18 +0000

    Add project files.

---
+++ Pages/GitHub.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET_CORE_WEB_APP.Pages.Shared
+{
+    public class GitHubModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
